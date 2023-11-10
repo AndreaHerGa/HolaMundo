aa8fbaf5dfe608a7d7deac66841b1b990286ca09
AndreaHerGa <150393754+AndreaHerGa@users.noreply.github.com>
2023-11-09 18:22:14 -0600

    Create holaMundo.py

---
+++ holaMundo.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Hola, mundo")
+st.write("Nombre: Andrea Hernández García")
+st.write("Carrera: Ingenieria en sistemas computacionales")
+st.write("No.control: 19091346")
+st.write("Edad: 22 años")
